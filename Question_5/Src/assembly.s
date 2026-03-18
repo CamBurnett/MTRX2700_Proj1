420e3f00e7093a4ecc6709a95ed24bd04dbd74a5
CamBurnett <cam@burnett.com.au>
2026-03-18 16:44:40 +1100

    Message Creation to be sent complete

--- Question_5/Src/assembly.s
+++ Question_5/Src/assembly.s
@@ -6,45 +6,150 @@
 .global main
 .thumb_func
 
+.data
+prefix: .asciz "COUNTER = "
+prefix_end:
+buffer: .space 32
+
+.align 2
+
 .type main, %function
 
 .text
 
 main:
-	@initialising
-	BL enable_peripheral_clocks @start peripheral clocks such that LEDs can work
-	BL initialise_discovery_board @initialising the board
-	BL enable_timer2_clock @start timer2 such that delay functions can be used
+    LDR R1, =prefix
+    MOV R2, #0
+    MOV R3, #0
+    @ Counter Value ---------------- TO CHANGE
+    MOV R4, #7
 
-	@initialising LEDs
-	LDR R7, =0b10110111 @loading register with which LEDs are turning on
-	LDR R0, =GPIOE @loading register with address of GPIOE
-	STRB R7, [R0, #ODR + 1] @loading the LED 'on' into GPIOE_ODR
+count_string:
+    @ Base length: STX(1) + MsgLen(1) + Prefix(10) + ETX(1) + Checksum(1) = 14
+    ADD R2, #14
 
-	BL delay
+check_digits:
+    CMP R4, #10
+    BLT single_digit
+    CMP R4, #100
+    BLT double_digit
+    B triple_digit
 
-	LDR R7, =0b00000000 @loading register with which LEDs are turning off
-	LDR R0, =GPIOE
-	STRB R7, [R0, #ODR + 1]
+single_digit:
+    ADD R2, #1
+    B done_digits
 
-stop:
-	B stop
+double_digit:
+    ADD R2, #2
+    B done_digits
 
-delay:
-	@initialising timer
-	LDR R1, =TIM2 @initialising timer 2
+triple_digit:
+    ADD R2, #3
+    B done_digits
 
-	MOV R2, #0 @putting 0 in register R2
-    STR R2, [R1, #TIM_CNT]   @ reset counter
+done_digits:
+    B concatenate
 
-	MOV R2, #1 @putting 1 in register R2
-	STR R2, [R1, #TIM_CR1] @storing counting timer
+concatenate:
+    LDR R6, =buffer
+    @Set R7 to buffer index
+    MOV R7, #0
 
-	LDR R3, =50000000 @picking max time to go
+    @ Set STX to first byte of buffer
+    MOV R5, #0x02
+    STRB R5, [R6, R7]
+    ADD R7, #1
 
-delay_loop:
-	LDR R5, [R1, #TIM_CNT] @loading the value of TIM_CMT into R5
-	CMP R5, R3 @compare the two values
-	BLT delay_loop
+    @ Set MSG Length to second byte of buffer
+    STRB R2, [R6, R7]
+    ADD R7, #1
 
-	BX LR
+    @ Set prefix bytes to next bytes of buffer
+    @Set R3 as index to add string bytes
+    MOV R3, #0
+copy_prefix:
+	@Loop through prefix and add to buffer
+    LDRB R5, [R1, R3]
+    CMP R5, #0
+    @If value is null, string is done
+    BEQ prefix_done
+    STRB R5, [R6, R7]
+    ADD R3, #1
+    ADD R7, #1
+    B copy_prefix
+
+prefix_done:
+    @Check if triple digit (R2 == 17)
+    CMP R2, #17
+    BLT skip_hundreds
+
+    MOV R5, R4
+    MOV R8, #0
+hundreds_loop:
+	@Obtain hundreds digit of the counter value
+    CMP R5, #100
+    BLT hundreds_done
+    SUB R5, R5, #100
+    ADD R8, #1
+    B hundreds_loop
+hundreds_done:
+	@Convert digit into ascii
+    ADD R8, #0x30
+    STRB R8, [R6, R7]
+    ADD R7, #1
+    MOV R4, R5
+
+skip_hundreds:
+    @Check if double digit or more (R2 >= 16)
+    CMP R2, #16
+    BLT skip_tens
+
+    MOV R5, R4
+    MOV R8, #0
+tens_loop:
+	@Obtain tens digit of the counter value
+    CMP R5, #10
+    BLT tens_done
+    SUB R5, R5, #10
+    ADD R8, #1
+    B tens_loop
+tens_done:
+	@Convert digit into ascii
+    ADD R8, #0x30
+    STRB R8, [R6, R7]
+    ADD R7, #1
+    MOV R4, R5
+
+skip_tens:
+    @Convert last digit into ascii
+    ADD R5, R4, #0x30
+    STRB R5, [R6, R7]
+    ADD R7, #1
+
+    @ Add ETX to end of string
+    MOV R5, #0x03
+    STRB R5, [R6, R7]
+    ADD R7, #1
+
+checksum:
+    @ Set R3 as checksum value
+    MOV R3, #0
+    @Set R8 as checksum index
+    MOV R8, #0
+checksum_loop:
+	@XOR every byte in the string to obtain checksum value
+    LDRB R5, [R6, R8]
+    EOR R3, R3, R5
+    ADD R8, #1
+    CMP R8, R7
+    BLT checksum_loop
+
+store_checksum:
+    STRB R3, [R6, R7]
+    ADD R7, #1
+
+end:
+	B end
+
+    @ R7 now holds total packet length
+    @ Buffer at R6 contains the complete packet
