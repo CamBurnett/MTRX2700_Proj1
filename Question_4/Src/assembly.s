60d57b27560b4103383842f67b73ce02e830e516
Chelsea Satriavi <csat0104@uni.sydney.edu.au>
2026-03-18 15:19:13 +1100

    draft of Question 4

--- Question_4/Src/assembly.s
+++ Question_4/Src/assembly.s
@@ -17,34 +17,140 @@ main:
 	BL enable_timer2_clock @start timer2 such that delay functions can be used
 
 	@initialising LEDs
-	LDR R7, =0b10110111 @loading register with which LEDs are turning on
-	LDR R0, =GPIOE @loading register with address of GPIOE
-	STRB R7, [R0, #ODR + 1] @loading the LED 'on' into GPIOE_ODR
+	@LDR R7, =0b10110111 @loading register with which LEDs are turning on
+	@LDR R0, =GPIOE @loading register with address of GPIOE
+	@STRB R7, [R0, #ODR + 1] @loading the LED 'on' into GPIOE_ODR
 
-	BL delay
+	@BL prescaler @starting the prescaler
+	@BL timer @branching into delay function using timer
 
-	LDR R7, =0b00000000 @loading register with which LEDs are turning off
+	@turning off LEDs
+	@LDR R7, =0b00000000 @loading register with which LEDs are turning off
+	@LDR R0, =GPIOE @loading register with address of GPIOE
+	@STRB R7, [R0, #ODR + 1] @loading the LED 'off' into GPIOE_ODR
+
+	LDR R0, =TIM2
+
+	@ prescaler: adjust this if needed for your board clock
+	@ if TIM2 clock = 8 MHz, PSC = 7 gives 1 tick = 1 us
+	MOV R1, #7
+	STR R1, [R0, #TIM_PSC]
+
+	@ load prescaler
+	BL trigger_prescaler
+
+	@ reset counter
+	MOV R1, #0
+	STR R1, [R0, #TIM_CNT]
+
+	@ start timer
+	MOV R1, #1
+	STR R1, [R0, #TIM_CR1]
+
 	LDR R0, =GPIOE
-	STRB R7, [R0, #ODR + 1]
+	MOV R1, #0
+	STRB R1, [R0, #ODR + 1]     @ all selected LEDs off initially
+
+	LDR R4, =200000             @ LED1 period
+	LDR R5, =700000             @ LED2 period
+
+	@ next toggle times
+	MOV R6, #0                  @ LED1 next toggle time
+	MOV R7, #0                  @ LED2 next toggle time
+
+main_loop:
+	@ read current timer count
+	LDR R0, =TIM2
+	LDR R1, [R0, #TIM_CNT]      @ current time
+
+	@ -----------------------------
+	@ LED1 check
+	@ if current time < next LED1 time, skip
+	@ else toggle LED1 and schedule next time
+	@ -----------------------------
+	CMP R1, R6
+	BLT skip_led1
+
+	LDR R0, =GPIOE
+	LDRB R2, [R0, #ODR + 1]
+	EOR R2, R2, #(1 << 0)       @ toggle PE8
+	STRB R2, [R0, #ODR + 1]
+
+	ADD R6, R6, R4              @ next LED1 toggle time
+
+skip_led1:
+	@ -----------------------------
+	@ LED2 check
+	@ if current time < next LED2 time, skip
+	@ else toggle LED2 and schedule next time
+	@ -----------------------------
+	LDR R0, =TIM2
+	LDR R1, [R0, #TIM_CNT]      @ read again just to be safe
+
+	CMP R1, R7
+	BLT skip_led2
+
+	LDR R0, =GPIOE
+	LDRB R2, [R0, #ODR + 1]
+	EOR R2, R2, #(1 << 1)       @ toggle PE9
+	STRB R2, [R0, #ODR + 1]
+
+	ADD R7, R7, R5              @ next LED2 toggle time
+
+skip_led2:
+	B main_loop
 
 stop:
-	B stop
+	B stop @stopping the file
 
-delay:
+timer:
 	@initialising timer
 	LDR R1, =TIM2 @initialising timer 2
 
-	MOV R2, #0 @putting 0 in register R2
-    STR R2, [R1, #TIM_CNT]   @ reset counter
+	@resetting the timer to zero
+	MOV R4, #0 @putting 0 in register R2
+    STR R4, [R1, #TIM_CNT]   @ reset counter
 
-	MOV R2, #1 @putting 1 in register R2
-	STR R2, [R1, #TIM_CR1] @storing counting timer
+	@storing counting timer
+	MOV R6, #1 @putting 1 in register R2
+	STR R6, [R1, #TIM_CR1] @storing counting timer
 
+	@creating loading function
+	LDR R8, =50000000 @setting value for when overflow
+	STR R8, [R1, TIM_ARR] @set the ARR register
+
+delay:
+	@picking delay length
 	LDR R3, =50000000 @picking max time to go
+
 
 delay_loop:
 	LDR R5, [R1, #TIM_CNT] @loading the value of TIM_CMT into R5
 	CMP R5, R3 @compare the two values
-	BLT delay_loop
 
-	BX LR
+	BLT delay_loop @loop if less than
+
+	BX LR @back to preceding function
+
+prescaler:
+	@storing a value for the prescaler
+	LDR R0, =TIM2 @load the base address for the timer
+	MOV R1, #0x02 @putting the prescaler value into R1
+	STR R1, [R0, TIM_PSC] @setting prescaler register
+
+trigger_prescaler:
+	LDR R0, =TIM2 @load the base address for the timer
+
+	LDR R1, =0x1 @setting value for when overflow
+	STR R1, [R0, TIM_ARR] @set the ARR register
+
+	MOV R2, #0 @loading value to R2
+	STR R2, [R0, #TIM_CNT] @resetting value of timer to 0
+
+	NOP @stalling for time
+	NOP @stalling for time
+
+	LDR R1, =0xffffffff @ set the ARR back to the default value
+	STR R1, [R0, TIM_ARR] @ set the ARR register
+
+	BX LR @back to prescaler
--- Question_4/Src/definitions.s
+++ Question_4/Src/definitions.s
@@ -8,12 +8,14 @@
 .equ ODR, 0x14	@ GPIO output register
 .equ MODER, 0x00 @ register for setting the port mode (in/out/etc)
 
-@clock and timing register
+@clock and timing registers
 .equ RCC, 0x40021000 @base register for resetting and clock settings
 .equ TIM2EN, 0 @enabling offset for timer2
 .equ TIM2, 0x40000000 @base register for the general timer2
 .equ TIM_CR1, 0x00 @control registers
-.equ TIM_CNT, 0x24  @ The actual counter location
+.equ TIM_CNT, 0x24  @the actual counter location
+.equ TIM_PSC, 0x28 @prescaler value
+.equ TIM_ARR, 0x2C @the register for the auto-reload
 
 @AHB registers for enabling clocks
 .equ AHBENR, 0x14 @enable peripherals
