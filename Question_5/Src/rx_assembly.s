810cf01e5ce61543a97a08916c1995c8104587a5
elenaz1234 <ezen0813@uni.sydney.edu.au>
2026-03-24 17:56:08 +1100

    added comments to question 5

--- Question_5/Src/rx_assembly.s
+++ Question_5/Src/rx_assembly.s
@@ -95,55 +95,55 @@ checksum_done:
 
 
 initialise_counter:
-	MOV R4, #0 @R4 stores the final numeric value
+	MOV R4, #0 @R4 stores the final numeric value and initialise to 0
 	MOV R10, #12 @R10 set to the first digit (offsets by 12)
 
 
 extract_number:
-	LDRB R3, [R6, R10]
-	CMP R3, #0x03
-	BEQ display_number
+	LDRB R3, [R6, R10]	@load the next ASCII character from the buffer into R3
+	CMP R3, #0x03	@Compare with 0x03 (end of the number)
+	BEQ display_number @if ETX, display result
 
 	SUB R3, R3, #0x30 @convert ASCII to numeric
 
-	MOV R2, #10
-	MUL R4, R4, R2
-	ADD R4, R4, R3
+	MOV R2, #10	 @Load 10 to R2 to shift decimal place
+	MUL R4, R4, R2	@Multiply current value by 10
+	ADD R4, R4, R3	@Add new digit to number
 
-	ADD R10, #1
-	B extract_number
+	ADD R10, #1	@Move to next character in buffer
+	B extract_number	@repeat loop for next digit
 
 display_number:
-	LDR R0, =GPIOE
-    STRB R4, [R0, #ODR + 1]
-    B send_ack
+	LDR R0, =GPIOE	@Load base address of GPIOE
+    STRB R4, [R0, #ODR + 1]	@Output lower 8 bits of R4 to LEDs
+    B send_ack	@Send ACK for valid message received
 
 bad_message:
-	BL flash_3
-    B send_nak
+	BL flash_3	@For errors call the function to flash LEDs 3 times
+    B send_nak	@Send NAK for malformed messages
 
 flash_3:
-	MOV R11, #6
+	MOV R11, #6	@Set loop counter to 6 for 3 flashes
     MOV R12, #0x00 @lights off initially
 
 flash_loop:
-	LDR R0, =GPIOE
-    STRB R12, [R0, #ODR + 1]
+	LDR R0, =GPIOE	@Load GPIOE base address
+    STRB R12, [R0, #ODR + 1]	@Load current on or off to LEDs
     EOR R12, R12, #0xFF @toggle all bits
 
-    BL delay_function
+    BL delay_function	@Delay to create flash
 
-    SUBS R11, R11, #1
-    BNE flash_loop
+    SUBS R11, R11, #1	@Decrease flash counter
+    BNE flash_loop	@Repeat 6 times
 
     BX LR
 
 delay_function:
-	LDR R5, =0x0FFFFF
+	LDR R5, =0x0FFFFF	@load value to create delay
 
 not_finished:
-	SUBS R5, R5, #1
-    BNE not_finished
+	SUBS R5, R5, #1	@Decrease delay counter
+    BNE not_finished	@Continue looping until counter reaches zero
 
     BX LR
 
